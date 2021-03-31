7b5e3aede8b39c6210935fd592422fafc2fe2f49
jim zhou <43537315+jimtje@users.noreply.github.com>
2021-03-31 01:41:51 -0700

    updated setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("requirements.txt") as f:
 
 setup(
     name="walletlib",
-    version="0.2.0",
+    version="0.2.1",
     packages=find_packages(),
     include_package_data=True,
     url="https://github.com/jimtje/walletlib",
