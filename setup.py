6950ceb4990482789afa6e220f170245e6a696cf
jim zhou <43537315+jimtje@users.noreply.github.com>
2022-02-20 10:00:47 -0800

    edits to readme, dockerfile

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="walletlib",
-    version="0.2.9",
+    version="0.2.10",
     packages=find_packages(),
     include_package_data=True,
     url="https://github.com/jimtje/walletlib",
